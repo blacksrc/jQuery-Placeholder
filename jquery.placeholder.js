e4f1e248af4f9d49e26285c4f812413483d68533
Siamak A.M <blackmaak@yahoo.com>
2014-07-15 18:16:10 +0000

    Create jquery.placeholder.js

---
+++ jquery.placeholder.js
@@ -0,0 +1,30 @@
+(function($) {
+    $.fn.placeholder = function(options) {
+    	var config = $.extend({
+			force    : false
+		}, options);
+    	var el = document.createElement('input');
+	    var support = ('placeholder' in el);
+		return this.each( function() {
+			if(config.force==true  || !support) {
+				$(this).each(function() {
+					var input = $(this);
+					var ph = $(this).attr('placeholder');
+					typeof(ph) != 'undefined' ? input.val(ph) : '';
+				});
+				$(this).focus(function() {
+					var input = $(this);
+					var vl = $.trim(input.val());
+					var ph = $(this).attr('placeholder');
+					vl == ph ? $(this).val('') : '';
+
+				}).blur(function() {
+					var input = $(this);
+					var vl = $.trim(input.val());
+					var ph = $(this).attr('placeholder');
+					(vl == '' && vl != ph) ? input.val(ph) : '';
+				});
+			}
+		});
+	}
+}(jQuery));
